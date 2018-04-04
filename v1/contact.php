d371725f15921b6d8a007d495ae6ede93e1ee52b
metillwick <32344288+metillwick@users.noreply.github.com>
2018-04-04 14:35:40 -0500

    Edit emails

--- v1/contact.php
+++ v1/contact.php
@@ -1,6 +1,6 @@
 <?php 
 if(isset($_POST['submit'])){
-    $to = "metillwick@unomaha.edu, ctienken@unomaha.edu, tmister2009@gmail.com"; // this is your Email address
+    $to = "<email>"; // this is your Email address
     $from = $_POST['email']; // this is the sender's Email address
     $name = $_POST['name'];
     $phone = $_POST['phone'];
